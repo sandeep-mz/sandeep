b1a0ef2d818c5383586554ab3fea9e308e244777
Sandeep Maurya <10millionvibration@gmail.com>
2024-03-14 02:22:22 +0530

    3nd commit

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from "react";
+import "../../style/about.css";
+import about from "../../images/about.png";
+import fire from "../../images/fire.png";
+import project from "../../images/project.png";
+import team from "../../images/team.png";
+import win from "../../images/win.png";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    
+    <section className=" about container section " id="about">
+      <h2>About Me</h2>
+      <div className="about-content ">
+        <div class="gallery"> 
+        <img src={about} alt="about-img" className="about-img" />
+        </div>
+        <div className="about-data ">
+          <div className="about-info">
+            <p className="about-discription">
+              I am deeply passionate about web development, and my unwavering
+              commitment to excellence, teamwork, and continuous personal and
+              professional growth drives my journey in this dynamic field. With
+              a thirst for knowledge and a relentless pursuit of innovation, I
+              thrive in the ever-evolving world of web technologies.
+            </p>
+            <a href="https://doc-04-6c-prod-01-apps-viewer.googleusercontent.com/viewer2/prod-01/pdf/g7i30kms4cvqug0ck0isqa8v17vecpu2/qdnttg12i6c67fr0l54mt7h3dlppa0rn/1710362475000/3/100258980398847536715/APznzaZVYpf4jaSE1Q548HN9wm0a9zfrneZeFjno6ljX3nSWeaXbIqi653veqzjtDaFNg2WtIKgVvJowqNaaX-SRkbjBmt66-8hy8AXHu5FDUXfkwIFfbkU75hq7XNAePo-HV_tFXWKQiGuJ1O8F_ZWAaS7JFmFiCdu7h3R_AIHF7geeto1kPkc_Ev5aSwIP6Ce5ta3bHCFzAHQyCsL-YU8CG69n6V9GV9yds3jQ54896piM5-ni8NXh5OCg07ZfsltgfR7dcjXLccXn5oaRKk-QXQQnmUmmSTKx6VHrDqrcNVPPH0dfdqa_HnY79026yMk5zGCFDO59DbVGPmu1PDrvZZozh-NZ0-36FscLdabKU8jq687BQUdpFNuFBu5i2hPuMNPDf4GnEi-LsYTowB5x5DSQvSzD50RTE84wAO9zrd0yb6iXNHs=?authuser=0&nonce=mb1tk3krn1ses&user=100258980398847536715&hash=lu5jdnqucm0dqs02gf9d88gbqhh1ouud" className="btn">
+              Download CV
+            </a>
+          </div>
+
+          <div className="about-skills ">
+            <div className="skills-data">
+              <div className="skills-title">
+                <h3 className="skills-name">Development</h3>
+                <span className="skills-number development">90%</span>
+              </div>
+              <div className="skills-bar">
+                <span className="skills-percentage"></span>
+              </div>
+            </div>
+
+            <div className="skills-data">
+              <div className="skills-title">
+                <h3 className="skills-name">UI/UX design</h3>
+                <span className="skills-number">80%</span>
+              </div>
+              <div className="skills-bar">
+                <span className="skills-percentage"></span>
+              </div>
+            </div>
+
+            <div className="skills-data">
+              <div className="skills-title">
+                <h3 className="skills-name">Programming</h3>
+                <span className="skills-number">75%</span>
+              </div>
+              <div className="skills-bar">
+                <span className="skills-percentage"></span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <div className="ido">
+        <div className="mz">
+          <img src={fire} alt="logo" />
+          <div className="mz-title">
+            <h3>1274+</h3>
+            <p>Coding Question Solved</p>
+          </div>
+        </div>
+
+        <div className="mz">
+          <img src={project} alt="logo" />
+          <div className="mz-title">
+            <h3>19+</h3>
+            <p>Projects Completed</p>
+          </div>
+        </div>
+
+        <div className="mz">
+          <img src={team} alt="logo" />
+          <div className="mz-title">
+            <h3>07+</h3>
+            <p>Team Project Complete</p>
+          </div>
+        </div>
+
+        <div className="mz">
+          <img src={win} alt="logo" />
+          <div className="mz-title">
+            <h3>49</h3>
+            <p>Heighest Streak in coding</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,6 +5,18 @@ import { FaLinkedin } from "react-icons/fa";
 import youtube from "../../images/youtube.jpg";
 import sportify from "../../images/spotify-clone.webp";
 import blinkit from "../../images/blinkit.webp";
+import excel from "../../images/excel.webp";
+import mzshooping from "../../images/shooping.png";
+import food from "../../images/food.png";
+import book from "../../images/book.png";
+import weather from "../../images/weather.png";
+import travel from "../../images/travel.png";
+import postoffice from "../../images/postoffice.png";
+import authentication from "../../images/authentication.avif";
+import submission from "../../images/submission.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -12,11 +24,11 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   const settings = {
-    dots: true,
+    // dots: true,
     infinite: false,
-    speed: 800,
+    speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
   };
 
   return (
@@ -35,7 +47,8 @@ const Home = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaLinkedin />Connect with LinkedIn{" "}
+            <FaLinkedin />
+            Connect with LinkedIn{" "}
           </a>
         </div>
       </div>
@@ -43,44 +56,221 @@ const Home = () => {
       <div className="recent-projects">
         <h2>Recent Projects</h2>
         <Slider {...settings}>
-          <div className="projects">
-            <img src={youtube} alt="Project 1" />
-            <h3>Title 1</h3>
-            <p>Description 1</p>
-          </div>
-          <div className="projects">
-            <img src={blinkit} alt="Project 2" />
-            <h3>Title 2</h3>
-            <p>Description 2</p>
-          </div>
-          <div className="projects">
-            <img src={sportify} alt="Project 3" />
-            <h3>Title 3</h3>
-            <p>Description 3</p>
-          </div>
-          <div className="projects">
-            <img src={youtube} alt="Project 4" />
-            <h3>Title 4</h3>
-            <p>Description 4</p>
-          </div>
-          <div className="projects">
-            <img src={youtube} alt="Project 5" />
-            <h3>Title 5</h3>
-            <p>Description 5</p>
-          </div>
-          <div className="projects">
-            <img src={youtube} alt="Project 6" />
-            <h3>Title 6</h3>
-            <p>Description 6</p>
-          </div>
+          <a href="https://sandeep-mz.github.io/Excel-clone/">
+            <div className="projects pro">
+              <img src={excel} alt="Project 1" />
+              <div className="title">
+                <h3>Google Sheet Clone</h3>
+                {/* <p><a href="https://github.com/sandeep-mz/Excel-clone/"></a>Code</p> */}
+              </div>
+              <p>
+                Incorporates DOM manipulation techniques to dynamically create,
+                modify, and delete multiple sheet, providing a responsive
+                experience to users.
+              </p>
+            </div>
+          </a>
+          <a href="https://sandeep-mz.github.io/blinkit-clone/">
+            <div className="projects pro">
+              <img src={blinkit} alt="Project 2" />
+              <h3>Blinkit Clone</h3>
+              <p>
+                Dive into core web development concepts by constructing a static
+                version of the Blinkit app. This project involves creating an
+                engaging interface using HTML and CSS.
+              </p>
+            </div>
+          </a>
 
-          <div className="projects">
-            <img src={youtube} alt="Project 6" />
-            <h3>Title 6</h3>
-            <p>Description 6</p>
-          </div>
+          <a href="https://sandeep-mz.github.io/Spotify-clone/index.html">
+            <div className="projects pro">
+              <img src={sportify} alt="Project 3" />
+              <h3>Spotify Clone</h3>
+              <p>
+                Creation of a Spotify clone using HTML and CSS, The objective is
+                to imitate the primary interfaces and functionality of the
+                Spotify web app..
+              </p>
+            </div>
+          </a>
 
+          <a href="https://sandeep-mz.github.io/YouTube-clone/">
+            <div className="projects pro">
+              <img src={youtube} alt="Project 4" />
+              <h3>Youtube Clone</h3>
+              <p>
+                Front-end developer proficient in HTML, CSS, JavaScript.In UI/UX
+                design and integrating APIs. Skilled in creating responsive web
+                applications with attention to detail.
+              </p>
+            </div>
+          </a>
         </Slider>
+      </div>
+
+      <div className="iknow">
+        <h2>Recent Projects</h2>
+        <div className="sections">
+          <div className="asection">
+            <h3>Front-End</h3>
+            <div className="tags">
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JavaScript</p>
+              <p>Bootstrap</p>
+              <p>React</p>
+              <p>Redux</p>
+            </div>
+          </div>
+          <div className="bsection">
+            <h3>Back-End</h3>
+            <div className="tags">
+              <p>PHP</p>
+              <p>Express.js</p>
+              <p>Python</p>
+              <p>Java Swing</p>
+            </div>
+          </div>
+          <div className="csection">
+            <h3>Database</h3>
+            <div className="tags">
+              <p>MySQL</p>
+            </div>
+          </div>
+
+          <div className="dsection">
+            <h3>Tools</h3>
+            <div className="tags">
+              <p>Github</p>
+              <p>Visual Studio Code</p>
+              <p>Visual Studio</p>
+              <p>NetBeans</p>
+              <p>Figma</p>
+              <p>Canvas</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="moreProject">
+        {/*------------------------ project 1st  */}
+        <div className="mzproject">
+          <div>
+            <img src={mzshooping} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>MzShop</h3>
+            <p>Html, CSS, JavaScript</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://shoppingcart-henna.vercel.app/" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+        {/* -----------------------project 2nd  */}
+        <div className="mzproject">
+          <div>
+            <img src={food} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>Food Order</h3>
+            <p>Html, CSS, JavaScript, APIs</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://foodsystem-steel.vercel.app/" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+         {/* -----------------------project 3nd  */}
+         <div className="mzproject">
+          <div>
+            <img src={book} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>Bookstore</h3>
+            <p>Html, CSS, JavaScript, APIs</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://bookstore-nu-ashen.vercel.app/" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+        {/* -----------------------project 4nd  */}
+        <div className="mzproject">
+          <div>
+            <img src={weather} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>Weather-city</h3>
+            <p>Html, CSS, JavaScript</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://city-weather-two.vercel.app" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+         {/* -----------------------project 5nd  */}
+         <div className="mzproject">
+          <div>
+            <img src={travel} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>Travel media</h3>
+            <p>Html, CSS, JavaScript, React</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://travelmedia.vercel.app" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+        {/* -----------------------project 6nd  */}
+        <div className="mzproject">
+          <div>
+            <img src={postoffice} alt="logo" />
+          </div>
+          <div className="projectTitle">
+            <h3>Post Office</h3>
+            <p>Html, CSS, JavaScript, React</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://sandeep-mz.github.io/postoffice/" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+        {/* -----------------------project 7nd  */}
+        <div className="mzproject">
+          <div>
+            <img src={authentication} alt="logo"/>
+          </div>
+          <div className="projectTitle">
+            <h3>Authentication</h3>
+            <p>Html, CSS, JavaScript, React</p>
+          </div>
+          <div className="mzicon">
+            <a href="https://authentication-nu-eight.vercel.app/" target="_blank">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </a>
+          </div>
+        </div>
+        {/* add more projects here */}
+
+      </div>
+
+      <div className="extra-mz">
+        <div className="pub">
+           <img src={submission} alt="logo" />
+          
+          <div className="extra-title">
+            <h3>Paper Publishing of BE Final Years Project</h3>
+            <p>Our team proudly announces the publication of our BE final year project research paper in Recent Innovations in <b>Wireless Network Security</b>. Highlighting key findings and comprehensive analysis, our paper delves into cutting-edge developments, evaluating security protocols, encryption techniques, and intrusion detection systems.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
