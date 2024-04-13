73fc9581dfff834f8a96301f36ec27c3b0960a99
Sandeep Maurya <10millionvibration@gmail.com>
2024-04-13 20:47:58 +0530

    update blog

--- src/components/pages/BlogData.jsx
+++ src/components/pages/BlogData.jsx
@@ -1,72 +1,96 @@
 const blogData = [
-    {
-      id: 1,
-      title: "Mistral Launches 8X22B an Experts AI Model",
-      author: "Akash Dutta",
-      date: "April 11, 2024",
-      content: "Mistral unveils 8X22B AI model, succeeding 8X7B, with expanded parameters. The new release promises enhanced capabilities and builds on previous advancements.",
-      imageUrl: "https://i.gadgets360cdn.com/large/artificial_intelligence_pexels_1712818733517.jpg?downsize=950:*",
-      readMoreUrl: "https://www.gadgets360.com/ai/news/mistral-8x22b-ai-mixture-experts-model-launch-open-source-benchmark-5418144",
-    },
-    {
-      id: 2,
-      title: "iPhone 17 Pro Models to Arrive in 2025",
-      author: "David Delima",
-      date: "April 11, 2024",
-      content: "Apple to launch 2nm processor iPhone in 2025, powered by TSMC. The new chipset marks a leap in performance and efficiency, according to reports.",
-      imageUrl: "https://i.gadgets360cdn.com/large/iphone15_pro_apple_1695640733704.jpg?downsize=950:*",
-      readMoreUrl: "https://www.gadgets360.com/mobiles/news/iphone-17-pro-max-apple-2nm-tsmc-chipset-report-5418907",
-    },
-    // Add more blog data as needed
-    {
-      id: 3,
-      title: "Facebook ads used by hackers",
-      author: "Sandeep Mz",
-      date: "April 11, 2024",
-      content: "Hackers are reportedly using Facebook ads to lure unsuspecting users to download AI powered tools like Midjourney, Sora and DALL-E that contain information stealing malware",
-      imageUrl: "https://i.gadgets360cdn.com/large/gemini_15_1712752869015.jpg?downsize=950:*",
-      readMoreUrl: "https://indianexpress.com/article/technology/tech-news-technology/hackers-using-facebook-ads-to-promote-fake-ai-tools-9263958/",
-    },
-    
-    {
-      id: 4,
-      title: "Oppo, OnePlus Collaborate With Google",
-      author: "Akash Dutta",
-      date: "April 11, 2024",
-      content: "Oppo, OnePlus partner with Google, integrating Gemini 1.0 Ultra AI model for innovative features, enhancing capabilities on their devices.",
-      imageUrl: "https://i.gadgets360cdn.com/large/oppo_google_ai_1712823787231.jpg?downsize=950:*",
-      readMoreUrl: "https://www.gadgets360.com/mobiles/news/oppo-oneplus-collaboration-google-gemini-ai-smartphones-tablets-5418597",
-    },
-    {
-        id: 5,
-        title: "World’s Most Powerful Open Source AI Model",
-        author: "Sandeep Mz",
-        date: "March 10, 2024",
-        content: "This past Monday, about a dozen engineers and executives at data science and AI company Databricks, connected via Zoom to learn if they had succeeded in building a top AI language model",
-        imageUrl: "https://media.wired.com/photos/660359d1c5394d5463ec7d5f/master/w_1920,c_limit/2024_03_25_Databricks_%20164.jpg",
-        readMoreUrl: "https://www.wired.com/story/dbrx-inside-the-creation-of-the-worlds-most-powerful-open-source-ai-model/",
-      },
-      {
-          id: 6,
-          title: "Top Career Options after BCA",
-          author: "Sandeep Mz",
-          date: "April 12, 2024",
-          content: "BCA or Bachelor of Computer Application is a three-year undergraduate course that imparts students knowledge of basic computing skills and software development.",
-          imageUrl: "https://www.simplilearn.com/ice9/free_resources_article_thumb/How_To_Become_A_Content_Write.jpg",
-          readMoreUrl: "https://www.simplilearn.com/career-options-after-bca-article",
-        },
-      
+  // {
+  //   id: 5,
+  //   title: "",
+  //   author: "Sandeep Mz",
+  //   date: "April 11, 2024",
+  //   content: "",
+  //   imageUrl: "",
+  //   readMoreUrl: "",
+  // },
+  {
+    id: 7,
+    title: "Neuralink's first human patient",
+    author: "Sandeep Mz",
+    date: "April 13, 2024",
+    content: "Elon Musk says the first person with a Neuralink brain-chip has recovered and can control a computer mouse with their thoughts.",
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSxBKQd1rdh-yRpNA4OzjtHZwUgm-E-D6x6KlDCKJiw&s",
+    readMoreUrl: "https://www.reuters.com/business/healthcare-pharmaceuticals/neuralinks-first-human-patient-able-control-mouse-through-thinking-musk-says-2024-02-20/",
+  },
 
-    // {
-    //   id: 5,
-    //   title: "",
-    //   author: "Sandeep Mz",
-    //   date: "April 11, 2024",
-    //   content: "",
-    //   imageUrl: "",
-    //   readMoreUrl: "",
-    // },
-  ];
-  
-  export default blogData;
-  
+  {
+    id: 6,
+    title: "Mistral Launches 8X22B an Experts AI Model",
+    author: "Akash Dutta",
+    date: "April 11, 2024",
+    content:
+      "Mistral unveils 8X22B AI model, succeeding 8X7B, with expanded parameters. The new release promises enhanced capabilities and builds on previous advancements.",
+    imageUrl:
+      "https://i.gadgets360cdn.com/large/artificial_intelligence_pexels_1712818733517.jpg?downsize=950:*",
+    readMoreUrl:
+      "https://www.gadgets360.com/ai/news/mistral-8x22b-ai-mixture-experts-model-launch-open-source-benchmark-5418144",
+  },
+  {
+    id: 5,
+    title: "iPhone 17 Pro Models to Arrive in 2025",
+    author: "David Delima",
+    date: "April 11, 2024",
+    content:
+      "Apple to launch 2nm processor iPhone in 2025, powered by TSMC. The new chipset marks a leap in performance and efficiency, according to reports.",
+    imageUrl:
+      "https://i.gadgets360cdn.com/large/iphone15_pro_apple_1695640733704.jpg?downsize=950:*",
+    readMoreUrl:
+      "https://www.gadgets360.com/mobiles/news/iphone-17-pro-max-apple-2nm-tsmc-chipset-report-5418907",
+  },
+  // Add more blog data as needed
+  {
+    id: 4,
+    title: "Facebook ads used by hackers",
+    author: "Sandeep Mz",
+    date: "April 11, 2024",
+    content:
+      "Hackers are reportedly using Facebook ads to lure unsuspecting users to download AI powered tools like Midjourney, Sora and DALL-E that contain information stealing malware",
+    imageUrl:
+      "https://i.gadgets360cdn.com/large/gemini_15_1712752869015.jpg?downsize=950:*",
+    readMoreUrl:
+      "https://indianexpress.com/article/technology/tech-news-technology/hackers-using-facebook-ads-to-promote-fake-ai-tools-9263958/",
+  },
+
+  {
+    id: 3,
+    title: "Oppo, OnePlus Collaborate With Google",
+    author: "Akash Dutta",
+    date: "April 11, 2024",
+    content:
+      "Oppo, OnePlus partner with Google, integrating Gemini 1.0 Ultra AI model for innovative features, enhancing capabilities on their devices.",
+    imageUrl:
+      "https://i.gadgets360cdn.com/large/oppo_google_ai_1712823787231.jpg?downsize=950:*",
+    readMoreUrl:
+      "https://www.gadgets360.com/mobiles/news/oppo-oneplus-collaboration-google-gemini-ai-smartphones-tablets-5418597",
+  },
+  {
+    id: 2,
+    title: "World’s Most Powerful Open Source AI Model",
+    author: "Sandeep Mz",
+    date: "March 10, 2024",
+    content:
+      "This past Monday, about a dozen engineers and executives at data science and AI company Databricks, connected via Zoom to learn if they had succeeded in building a top AI language model",
+    imageUrl:
+      "https://media.wired.com/photos/660359d1c5394d5463ec7d5f/master/w_1920,c_limit/2024_03_25_Databricks_%20164.jpg",
+    readMoreUrl:
+      "https://www.wired.com/story/dbrx-inside-the-creation-of-the-worlds-most-powerful-open-source-ai-model/",
+  },
+  {
+    id: 1,
+    title: "Top Career Options after BCA",
+    author: "Sandeep Mz",
+    date: "April 12, 2024",
+    content:
+      "BCA or Bachelor of Computer Application is a three-year undergraduate course that imparts students knowledge of basic computing skills and software development.",
+    imageUrl:
+      "https://www.simplilearn.com/ice9/free_resources_article_thumb/How_To_Become_A_Content_Write.jpg",
+    readMoreUrl: "https://www.simplilearn.com/career-options-after-bca-article",
+  },
+];
+
+export default blogData;
